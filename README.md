f0f1fadea58f330b192c3be8a17b7a723266fa35
ccckmit <ccckmit@gmail.com>
2017-06-20 16:20:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# c6e -- Chart GUI for node.js (based on Electron.js)
+
+## Install
+
+1. Make sure you have install Electron.js in global mode.
+2. Git clone this project and install it.
+
+```
+$ npm install -g electron
+$ npm install -g c6e
+```
+
+## Demo
+
+```
+$ git clone https://github.com/ccckmit/c6demo.git
+$ cd c6demo
+$ npm install
+```
+
+
+Example 1 : 
+
+```
+$ c6e chartDemo.js
+```
+
+![](img/chartDemo.png)
---
+++ c6e.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var runFile = process.argv[2]
+var jsFile, htmlFile
+if (runFile.indexOf('.html') < 0) {
+  htmlFile = 'c6e.html'
+  jsFile = runFile
+} else {
+  htmlFile = runFile
+  jsFile = process.argv[3]
+}
+console.log('argv = %j', process.argv)
+var exec = require('child_process').exec
+var child = exec(`electron ${__dirname} ${htmlFile} ${jsFile}`, function (err, stdout, stderr) {
+  if (err) throw err
+  console.log(stdout)
+})
